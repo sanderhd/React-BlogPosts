c177fc308f4091f856f29cb66f72bd807576f238
SanderHoudijk <fristislekker@proton.me>
2025-02-16 17:23:02 +0100

    Verwijder de het onodige "Posts.json" bestand. Fix 1 error in packages.json. Voeg TailwindCSS toe aan gehele project.

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 function Home() {
     return (
         <div>
-            <h1> Welkom op mijn site :)</h1>
+            <h1 className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+                Welkom op mijn site :)
+            </h1>
         </div>
     );
 }
